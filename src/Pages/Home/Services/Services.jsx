784d604ab31b9c592e451972881c5c2525739bb4
Md Sirajul Munir <abc@gmail.com>
2023-12-06 09:12:50 +0600

    Service data is ready

---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,17 @@
+
+const Services = () => {
+    return (
+        <div className="mt-4">
+            <div className="text-center">
+                <h3 className="text-2xl font-bold text-orange-400">Our Services</h3>
+                <h2 className="text-5xl">Our Service </h2>
+                <p>The majority have suffered alteration in some form, 
+                    by injected humour or randomized<br/> words which 
+                    do not look even slightly believable
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
